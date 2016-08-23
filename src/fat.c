6d21675248bbd5fe6cabcfaca3b6fae5c3ed7041
Luis de Bethencourt <luisbg@osg.samsung.com>
2016-08-23 18:56:21 +0100

    fat: remove support for non-fat32
    Only support FAT32 and simplify the code. No need for code that handles
    FAT12 and FAT16.

--- src/fat.c
+++ src/fat.c
@@ -43,33 +43,18 @@
  */
 void get_fat(FAT_ENTRY * entry, void *fat, uint32_t cluster, DOS_FS * fs)
 {
-    unsigned char *ptr;
+    uint32_t e;
 
     if (cluster > fs->data_clusters + 1) {
         die("Internal error: cluster out of range in get_fat() (%lu > %lu).", (unsigned long) cluster, (unsigned long) (fs->data_clusters + 1));
     }
 
-    switch (fs->fat_bits) {
-    case 12:
-        ptr = &((unsigned char *) fat)[cluster * 3 / 2];
-        entry->value = 0xfff & (cluster & 1 ? (ptr[0] >> 4) | (ptr[1] << 4)
-                                : (ptr[0] | ptr[1] << 8));
-        break;
-    case 16:
-        entry->value = le16toh(((unsigned short *) fat)[cluster]);
-        break;
-    case 32:
-        /* According to M$, the high 4 bits of a FAT32 entry are reserved and
-         * are not part of the cluster number. So we cut them off. */
-        {
-            uint32_t e = le32toh(((unsigned int *) fat)[cluster]);
-            entry->value = e & 0xfffffff;
-            entry->reserved = e >> 28;
-        }
-        break;
-    default:
-        die("Bad FAT entry size: %d bits.", fs->fat_bits);
-    }
+    printf("lbg: here\n");
+    /* According to M$, the high 4 bits of a FAT32 entry are reserved and
+     * are not part of the cluster number. So we cut them off. */
+    e = le32toh(((unsigned int *) fat)[cluster]);
+    entry->value = e & 0xfffffff;
+    entry->reserved = e >> 28;
 }
 
 /**
@@ -99,13 +84,7 @@ void read_fat(DOS_FS * fs)
     total_num_clusters = fs->data_clusters + 2;
     eff_size = (total_num_clusters * fs->fat_bits + 7) / 8ULL;
 
-    if (fs->fat_bits != 12)
-        alloc_size = eff_size;
-    else
-        /* round up to an even number of FAT entries to avoid special
-         * casing the last entry in get_fat() */
-        alloc_size = (total_num_clusters * 12 + 23) / 24 * 3;
-
+    alloc_size = eff_size;
     first = alloc(alloc_size);
     fs_read(fs->fat_start, eff_size, first);
     if (fs->nfats > 1) {
